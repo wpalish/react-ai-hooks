bbb653daba1f2988bb9f0928afb3587e85492735
Алишер Нурсаин <alisher.nursain@gmail.com>
2026-06-10 14:52:26 +0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { useChat } from './useChat';
+export { useCompletion } from './useCompletion';
+export { useStream, readStream } from './useStream';
+export type { Message } from './useChat';
